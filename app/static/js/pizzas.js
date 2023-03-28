8321aaccec09bcb78d293169e8d228347955c261
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2023-03-28 19:48:42 -0300

    first commit

---
+++ app/static/js/pizzas.js
@@ -0,0 +1,146 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Ração Champ',
+        img: 'images/raçao1.png',
+        price: [45.00, 85.00, 125.00],
+        sizes: [
+          '2kg',
+          '10kg',
+          '20kg'
+        ],
+        description: 'Carne & cereal'
+      },
+      {
+        id: 2,
+        name: 'Ração Pedigree',
+        img: 'images/raçao2.png',
+        price: [45.00, 85.00, 125.00],
+        sizes: [
+          '2kg',
+          '10kg',
+          '20kg'
+        ],
+        description: 'Seleção especial raças pequenas'
+      },
+      {
+        id: 3,
+        name: 'Ração Cibau',
+        img: 'images/raçao3.png',
+        price: [45.00, 85.00, 125.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Seleção especial filhotes'
+      },
+      {
+        id: 4,
+        name: 'Ração Whiskas',
+        img: 'images/raçao8.png',
+        price: [45.00, 85.00, 125.00],
+        sizes: [
+          '2kg',
+          '10kg',
+          '20kg'
+        ],
+        description: 'Frango e leite'
+      },
+      {
+        id: 5,
+        name: 'Ração Purina Friskies',
+        img: 'images/raçao5.png',
+        price: [45.00, 85.00, 125.00],
+        sizes: [
+          '2kg',
+          '10kg',
+          '20kg'
+        ],
+        description: 'Seleção especial'
+      },
+      {
+        id: 6,
+        name: 'Ração Golden',
+        img: 'images/raçao6.png',
+        price: [45.00, 85.00, 125.00],
+        sizes: [
+          '2kg',
+          '10kg',
+          '20kg'
+        ],
+        description: 'Peru e arroz'
+      },
+      {
+        id: 7,
+        name: 'Ração Alcon',
+        img: 'images/raçao7.png',
+        price: [10.00, 20.00, 35.00],
+        sizes: [
+          '50g',
+          '100g',
+          '250g'
+        ],
+        description: 'Spirulina'
+      },
+      {
+        id: 8,
+        name: 'Ração Alcon',
+        img: 'images/produto1.png',
+        price: [10.00, 20.00, 35.00],
+        sizes: [
+          '50g',
+          '100g',
+          '250g'
+        ],
+        description: 'Basic tabs'
+      },
+      {
+        id: 9,
+        name: 'Bebedouro para Passarinhos',
+        img: 'images/produto2.png',
+        price: [20.00, 40.00, 60.00],
+        sizes: [
+          '10cm',
+          '20cm',
+          '30cm'
+        ],
+        description: 'Azul'
+      },
+      {
+        id: 10,
+        name: 'Coleira Multi Patas',
+        img: 'images/produto3.png',
+        price: [10.00, 20.00, 30.00],
+        sizes: [
+          '10cm',
+          '20cm',
+          '30cm'
+        ],
+        description: 'Estampa tatuagens'
+      },
+      {
+        id: 11,
+        name: 'Comedouro para Cachorros',
+        img: 'images/produto4.png',
+        price: [10.00, 20.00, 30.00],
+        sizes: [
+          '2kg',
+          '5kg',
+          '10kg'
+        ],
+        description: 'Estampado azul'
+      },
+      {
+        id: 12,
+        name: 'Pijama para Cachorro',
+        img: 'images/produto5.png',
+        price: [10.00, 20.00, 30.00],
+        sizes: [
+          '10cm',
+          '20cm',
+          '30cm'
+        ],
+        description: 'Estampado azul com estrelas'
+      }
+];
