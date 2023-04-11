376a3bf1e3662d3ff2366869b8866b48db1fca5e
Jaime <jwcs2@aluno.ifal.edu.br>
2023-04-11 11:20:52 -0300

    sasas

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 SEX_CHOICES = (
     ('F', 'F'),
     ('M', 'M'),
-    
+)
 
 #OK
 class cliente(models.Model):
--- app/views.py
+++ app/views.py
@@ -54,3 +54,8 @@ def dologin(request):
         data['class'] = 'alert-danger'
         return render(request,'login.html',data)
 
+
+
+def logouts(request):
+    logout(request)
+    return redirect('/home/')
